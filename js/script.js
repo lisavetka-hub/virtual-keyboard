810f0740bd58d9feaf557361d386d8d3d954ca31
unknown <lizhen232002@gmail.com>
2020-04-06 16:21:42 +0300

    fix: key press track

--- js/script.js
+++ js/script.js
@@ -1,70 +1,3 @@
-// const keyboard = [
-//   113,
-//   119,
-//   101,
-//   114,
-//   116,
-//   121,
-//   117,
-//   105,
-//   111,
-//   112,
-//   91,
-//   93,
-//   97,
-//   115,
-//   100,
-//   102,
-//   103,
-//   104,
-//   106,
-//   107,
-//   108,
-//   59,
-//   39,
-//   122,
-//   120,
-//   99,
-//   118,
-//   98,
-//   110,
-//   109,
-//   44,
-//   46,
-//   47
-// ];
-// // document.onkeypress = function(event) {
-// //   // console.log(event);
-// //   keyboard.push(event.charCode);
-// //   console.log(keyboard);
-// // };
-// function init() {
-//   let out = "";
-//   for (let i = 0; i < keyboard.length; i++) {
-//     if (i == 12 || i == 23) {
-//       out += '<div class="clearfix"></div>';
-//     }
-//     out +=
-//       '<div class="k-key" data="' +
-//       keyboard[i] +
-//       '">' +
-//       String.fromCharCode(keyboard[i]) +
-//       "</div>";
-//   }
-//   document.querySelector("#keyboard").innerHTML = out;
-// }
-// init();
-
-// document.onkeypress = function(event) {
-//   console.log(event.code);
-//   console.log(event.keyCode);
-//   document.querySelectorAll("#keyboard .k-key").forEach(function(element) {
-//     element.classList.remove("active");
-//   });
-//   document
-//     .querySelector('#keyboard .k-key[data = "' + event.keyCode + '"]')
-//     .classList.add("active");
-// };
 const keyboard = {
   elements: {
     main: null,
@@ -88,6 +21,10 @@ const keyboard = {
     this.elements.main.classList.add("keyboard", "keyboard--hidden");
     this.elements.keysContainer.classList.add("keyboard__keys");
     this.elements.keysContainer.appendChild(this._createKeys());
+
+    this.elements.keys = this.elements.keysContainer.querySelectorAll(
+      ".keyboard__key"
+    );
 
     // Add to DOM
     this.elements.main.appendChild(this.elements.keysContainer);
@@ -160,14 +97,26 @@ const keyboard = {
         console.log(event.code); //keyF
         console.log(event.key); //F
 
-        for (var i = 0; i < keyLayout.length; i++) {
+        for (var i = 1; i < keyLayout.length; i++) {
           if (event.key == keyLayout[i]) {
-            console.log("Продолжаем!");
-            keyElement.classList.add("active");
+            var searchTag = document.getElementsByTagName("button");
+            var searchText = keyLayout[i];
+            var found;
+            for (var t = 0; t < searchTag; t++) {
+              if (searchTag[t].textContent == searchText) {
+                found = searchTag[t];
+                break;
+              }
+            }
+
+            //console.log("it's working!");
+
+            document.getElementsByClassName(
+              "keyboard__key"
+            )[0].style.backgroundColor = "red";
           }
         }
       };
-
       switch (key) {
         case "backspace":
           keyElement.classList.add("keyboard__key--wide");
@@ -255,7 +204,7 @@ const keyboard = {
     console.log("event Triggered! event name:" + hendlerName);
   },
   _toggleCapsLock() {
-    console.log("caps Lock toggled");
+    this.properties.capsLock = !this.properties.capsLock;
   },
   open(initialValue, oninput, oneclose) {},
   close() {},
